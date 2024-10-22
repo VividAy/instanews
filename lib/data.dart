c6776d71072be76a05f4f6731a93327968ef11a2
VividAy <102046252+VividAy@users.noreply.github.com>
2024-10-22 17:06:36 -0400

    heart works + titles

--- lib/data.dart
+++ lib/data.dart
@@ -1,65 +1,78 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-import 'dart:io'; // For working with File
-import 'package:provider/provider.dart';
 
-class datapkg{
-  String i, des;
-  datapkg(this.i, this.des);
+class datapkg {
+  String i, des, title;
+  datapkg(this.i, this.des, this.title);
 
-  String getImageURL(){
+  String getImageURL() {
     return this.i;
   }
-  String getDescription(){
+
+  String getDescription() {
     return this.des;
+  }
+
+  String getTitle() {
+    return this.title;
   }
 }
 
-class dataStorage{
+class dataStorage {
   dataStorage();
 
   List<String> newImages = [
-      // Add 10 image URLs here for demonstration
-      
-    ];
+    // Add 10 image URLs here for demonstration
+  ];
 
-   /* List<String> newDescriptions = [
-     ,
-      ,
-      ,
-      'A field of colorful wildflowers stretches as far as the eye can see, each bloom adding a vibrant touch to the landscape. The variety of red, yellow, and purple flowers sways gently with the breeze under a clear sky. It’s a scene that embodies the joy and renewal of spring, inviting viewers to immerse themselves in the beauty of nature.',
-      'A lone tree stands defiantly against a dark and stormy sky, its branches swaying in the wind. The dramatic clouds gather ominously above, hinting at a powerful storm brewing. This image captures the resilience of nature amidst adversity, reminding viewers of the strength and perseverance required to weather life’s challenges.',
-      'A snow-covered mountain peak rises majestically against a backdrop of golden morning light. The first rays of dawn touch the summit, creating a stunning contrast between the white snow and the warm, golden hues. It’s a breathtaking scene that symbolizes new beginnings and the beauty that lies in remote, untouched places.',
-      'The beach is bathed in the soft glow of dusk, with gentle waves rolling onto the sandy shore. The horizon blends seamlessly with the colors of the setting sun, creating a peaceful and calming atmosphere. This scene is perfect for those who love the ocean, offering a serene escape from the demands of daily life.',
-      'A thick fog blankets a dense forest, creating an air of mystery and intrigue. The trees fade into the mist, giving the impression that the forest stretches infinitely. This image evokes a sense of wonder and curiosity, as if inviting viewers to step into an unexplored world where anything might be possible.',
-      'The night sky comes alive with a stunning display of stars over a quiet desert landscape. The Milky Way arcs across the sky, its glow highlighting the vastness of the universe. The serene and desolate desert below contrasts with the brilliance above, making this scene a reminder of our place in the cosmos.',
-      'A quaint village nestled in rolling hills is adorned with vibrant autumn colors. The golden and red leaves create a warm and inviting atmosphere, with cottages dotting the landscape. This scene captures the charm of rural life in autumn, where nature and community come together to create a picturesque and comforting setting.',
-    ];*/
-  static List<datapkg> data = [datapkg('https://media.cnn.com/api/v1/images/stellar/prod/ap24294169788999-copy.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp', 'This image captures a stunning sunset over a mountain range, where the sky is painted with vibrant orange and pink hues. The shadows of the peaks create a dramatic contrast, making the scene look like a masterpiece of nature. It evokes a sense of tranquility and awe, perfect for those who appreciate nature’s beauty.'),
-      datapkg('https://picsum.photos/300/200?image=2','A serene lake mirrors the surrounding forest and sky in this tranquil scene. The water’s surface is so still that the trees and clouds create a near-perfect reflection, blending reality with illusion. This peaceful setting invites viewers to pause, reflect, and enjoy the natural beauty away from the hustle and bustle of daily life.'),
-      datapkg('https://picsum.photos/300/200?image=3','The image showcases a bustling cityscape during nighttime, with streets reflecting the vibrant lights of the skyscrapers. Neon signs and glowing windows illuminate the scene, giving a sense of the city’s energy and life. This visual captures the fast-paced rhythm of urban life, highlighting the contrast between the bright lights and the dark sky.'),
-      datapkg('https://picsum.photos/300/200?image=4', 'a'),
-      datapkg('https://picsum.photos/300/200?image=5', 'aa'),
-      datapkg('https://picsum.photos/300/200?image=6', 'aa'),
-      datapkg('https://picsum.photos/300/200?image=7', 'aa'),
-      datapkg('https://picsum.photos/300/200?image=8', 'aa'),
-      datapkg('https://picsum.photos/300/200?image=9', 'aa'),
-      datapkg('https://picsum.photos/300/200?image=10', 'aa'),];
+  static List<datapkg> data = [
+    datapkg(
+      'https://media.cnn.com/api/v1/images/stellar/prod/ap24294169788999-copy.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp',
+      'This image captures a stunning sunset over a mountain range, where the sky is painted with vibrant orange and pink hues. The shadows of the peaks create a dramatic contrast, making the scene look like a masterpiece of nature. It evokes a sense of tranquility and awe, perfect for those who appreciate nature’s beauty.',
+      'Sunset over the Mountains',
+    ),
+    datapkg(
+      'https://picsum.photos/300/200?image=2',
+      'A serene lake mirrors the surrounding forest and sky in this tranquil scene. The water’s surface is so still that the trees and clouds create a near-perfect reflection, blending reality with illusion. This peaceful setting invites viewers to pause, reflect, and enjoy the natural beauty away from the hustle and bustle of daily life.',
+      'Reflection at the Lake',
+    ),
+    datapkg(
+      'https://picsum.photos/300/200?image=3',
+      'The image showcases a bustling cityscape during nighttime, with streets reflecting the vibrant lights of the skyscrapers. Neon signs and glowing windows illuminate the scene, giving a sense of the city’s energy and life. This visual captures the fast-paced rhythm of urban life, highlighting the contrast between the bright lights and the dark sky.',
+      'Nighttime Cityscape',
+    ),
+    datapkg('https://picsum.photos/300/200?image=4', 'Description for image 4',
+        'Title 4'),
+    datapkg('https://picsum.photos/300/200?image=5', 'Description for image 5',
+        'Title 5'),
+    datapkg('https://picsum.photos/300/200?image=6', 'Description for image 6',
+        'Title 6'),
+    datapkg('https://picsum.photos/300/200?image=7', 'Description for image 7',
+        'Title 7'),
+    datapkg('https://picsum.photos/300/200?image=8', 'Description for image 8',
+        'Title 8'),
+    datapkg('https://picsum.photos/300/200?image=9', 'Description for image 9',
+        'Title 9'),
+    datapkg('https://picsum.photos/300/200?image=10',
+        'Description for image 10', 'Title 10'),
+  ];
 
-  void addData(String img, String desc){
-    data.add(datapkg(img, desc));
+  void addData(String img, String desc, String title) {
+    data.add(datapkg(img, desc, title));
   }
 
-  void setDesc(int index, String desc){
-    data[index] = datapkg(data[index].i, desc);
+  void setDesc(int index, String desc) {
+    data[index] = datapkg(data[index].i, desc, data[index].title);
   }
 
-  void setImg(int index, String img){
-    data[index] = datapkg(img, data[index].des);
+  void setImg(int index, String img) {
+    data[index] = datapkg(img, data[index].des, data[index].title);
   }
 
-  datapkg getData(int index){
+  void setTitle(int index, String title) {
+    data[index] = datapkg(data[index].i, data[index].des, title);
+  }
+
+  datapkg getData(int index) {
     return data.elementAt(index);
   }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart'; // For launching URLs in a browser
 import 'profile_page.dart';
@@ -15,44 +13,56 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   dataStorage datastore = dataStorage();
   List<String> _imageUrls = [];
+  List<String> _titles = [];
   List<String> _descriptions = [];
   final PageController _pageController =
       PageController(); // Controller for PageView
   String _selectedItem = 'Literature'; // Track the selected top bar item
   int _currentPage = 0;
-  int _likeState = 0;
-  int liked = 0;
+  List<bool> _likeStates = []; // Track like states for each post
+  List<int> _likeCounts = []; // Track the number of likes for each post
+
   @override
   void initState() {
     super.initState();
-    _loadImagesAndDescriptions(); // Load the initial set of images and descriptions
+    _loadImagesTitlesAndDescriptions(); // Load the initial set of images, titles, and descriptions
   }
 
-  // Function to load 10 images and their descriptions at a time
-  void _loadImagesAndDescriptions() {
+  // Function to load 10 images, titles, and their descriptions at a time
+  void _loadImagesTitlesAndDescriptions() {
     List<String> newImages = [];
-
+    List<String> newTitles = [];
     List<String> newDescriptions = [];
-    for(int i = 0; i < 10; i++){
+    List<bool> newLikeStates = [];
+    List<int> newLikeCounts = [];
+
+    for (int i = 0; i < 10; i++) {
       newImages.add(datastore.getData(i).i);
+      newTitles.add(datastore.getData(i).title);
       newDescriptions.add(datastore.getData(i).des);
+      newLikeStates.add(false); // Initialize all new items as not liked
+      newLikeCounts
+          .add(0); // Initialize the like count as zero for each new item
     }
-    
-    // Append the new images and descriptions to the current lists
+
+    // Append the new images, titles, descriptions, like states, and counts to the current lists
     setState(() {
       _imageUrls.addAll(newImages);
+      _titles.addAll(newTitles);
       _descriptions.addAll(newDescriptions);
+      _likeStates.addAll(newLikeStates);
+      _likeCounts.addAll(newLikeCounts);
     });
   }
 
-  // Fetch more images and descriptions once the user reaches the 10th page
+  // Fetch more images, titles, and descriptions once the user reaches the 10th page
   void _onPageChanged(int index) {
     _currentPage = index;
     if (_currentPage == _imageUrls.length - 1) {
-      _loadImagesAndDescriptions(); // Load the next set of 10 images and descriptions when the user reaches the last image
+      _loadImagesTitlesAndDescriptions(); // Load the next set of 10 images, titles, and descriptions when the user reaches the last image
     }
   }
-  // Initialize like count
+
   // Function to show a dialog when swiping right
   Future<void> _showLeaveDialog() async {
     bool shouldLeave = await showDialog(
@@ -104,8 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             // Horizontal scrolling top bar
             Padding(
-              padding:
-                  const EdgeInsets.only(top: 65.0), // Only top padding here
+              padding: const EdgeInsets.only(top: 65.0),
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
@@ -121,19 +130,16 @@ class _MyHomePageState extends State<MyHomePage> {
             // PageView for swipe-up containers
             Expanded(
               child: PageView.builder(
-                scrollDirection: Axis.vertical, // Enable vertical scrolling
+                scrollDirection: Axis.vertical,
                 controller: _pageController,
-                onPageChanged:
-                    _onPageChanged, // Detect when the user changes page
+                onPageChanged: _onPageChanged,
                 itemBuilder: (context, index) {
-                  // If no more images are available, display a loading indicator
                   if (index >= _imageUrls.length) {
                     return Center(child: CircularProgressIndicator());
                   }
-                  return _buildPage(index); // Build each page
+                  return _buildPage(index);
                 },
-                itemCount:
-                    _imageUrls.length + 1, // Add 1 for the loading indicator
+                itemCount: _imageUrls.length + 1,
               ),
             ),
           ],
@@ -152,8 +158,7 @@ class _MyHomePageState extends State<MyHomePage> {
         });
       },
       child: Padding(
-        padding: const EdgeInsets.symmetric(
-            horizontal: 7.0), // Horizontal padding between items
+        padding: const EdgeInsets.symmetric(horizontal: 7.0),
         child: Text(
           label,
           style: TextStyle(
@@ -168,123 +173,130 @@ class _MyHomePageState extends State<MyHomePage> {
 
   // Function to build each page for PageView
   Widget _buildPage(int index) {
-  List<String> _comments = []; // List to hold comments
-  TextEditingController _commentController = TextEditingController(); // Text controller for comments
-
-  return Column(
-    children: [
-      // First Container (showing an image)
-      Padding(
-        padding: const EdgeInsets.fromLTRB(16, 30, 16, 0),
-        child: Container(
-          width: double.infinity, // Make it take up full width
-          height: 300, // Specify height for the first container
-          decoration: BoxDecoration(
-            color: Colors.grey[300], // Grey color for the box
-            borderRadius: BorderRadius.circular(16.0), // Rounded corners
-          ),
-          child: _imageUrls[index].isNotEmpty
-              ? ClipRRect(
-                  borderRadius: BorderRadius.circular(
-                      16.0), // Rounded edges for the image
-                  child: Image.network(
-                    _imageUrls[index],
-                    fit: BoxFit.cover,
-                    loadingBuilder: (context, child, loadingProgress) {
-                      if (loadingProgress == null) {
-                        return child;
-                      }
-                      return Center(
-                        child: CircularProgressIndicator(
-                          value: loadingProgress.expectedTotalBytes != null
-                              ? loadingProgress.cumulativeBytesLoaded /
-                                  (loadingProgress.expectedTotalBytes ?? 1)
-                              : null,
-                        ),
-                      );
-                    },
-                  ),
-                )
-              : const Center(child: Text('No image available')),
-        ),
-      ),
-      const SizedBox(height: 16), // Add some space between the containers
-      // Second Container (showing the description, profile, likes, and comments)
-      Expanded(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-              horizontal: 16.0), // Uniform left and right padding
+    return Column(
+      children: [
+        // First Container (showing an image)
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16, 30, 16, 0),
           child: Container(
-            width: double.infinity, // Make it take up full width
+            width: double.infinity,
+            height: 300,
             decoration: BoxDecoration(
-              color: Colors.grey[300], // Grey color for the second container
-              borderRadius: BorderRadius.circular(16.0), // Rounded corners
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(16.0),
             ),
-            child: Stack(
-              children: [
-                // Main content in the center
-                Padding(
-                  padding: const EdgeInsets.all(
-                      16.0), // Uniform padding inside the description container
-                  child: Center(
-                    child: Text(
-                      _descriptions[index],
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
+            child: _imageUrls[index].isNotEmpty
+                ? ClipRRect(
+                    borderRadius: BorderRadius.circular(16.0),
+                    child: Image.network(
+                      _imageUrls[index],
+                      fit: BoxFit.cover,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if (loadingProgress == null) {
+                          return child;
+                        }
+                        return Center(
+                          child: CircularProgressIndicator(
+                            value: loadingProgress.expectedTotalBytes != null
+                                ? loadingProgress.cumulativeBytesLoaded /
+                                    (loadingProgress.expectedTotalBytes ?? 1)
+                                : null,
+                          ),
+                        );
+                      },
                     ),
-                  ),
-                ),
-                // Profile picture (bottom left)
-                Positioned(
-                  bottom: 10,
-                  left: 10,
-                  child: GestureDetector(
-                    onTap: () {
-                      // Navigate to another page (e.g., ProfilePage)
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const ProfilePage(),
+                  )
+                : const Center(child: Text('No image available')),
+          ),
+        ),
+        const SizedBox(height: 16),
+        // Second Container (showing the title, description, profile, likes, and comments)
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          _titles[index],
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      );
-                    },
-                    child: CircleAvatar(
-                      radius: 20, // Set the size of the profile picture
-                      backgroundImage: NetworkImage(
-                          'https://picsum.photos/200'), // Example profile image
+                        const SizedBox(height: 8),
+                        Text(
+                          _descriptions[index],
+                          style: const TextStyle(fontSize: 16),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                // Heart icon (bottom right)
-                Positioned(
-                  bottom: 10,
-                  right: 10,
-                  child: Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.favorite),
-                        color: Colors.red, // Color of the heart icon
-                        onPressed: () {
-                          setState(() {
-                            if(_likeState == 0){
-                              _likeState++; // Increment the like count
-                              liked = 1;
-                            }
-                          });
-                        },
+                  // Profile picture (bottom left)
+                  Positioned(
+                    bottom: 10,
+                    left: 10,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ProfilePage(),
+                          ),
+                        );
+                      },
+                      child: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: NetworkImage(
+                          'https://picsum.photos/200',
+                        ),
                       ),
-                      Text('$_likeState'),
-                    ],
+                    ),
                   ),
-                ),
-              ],
+                  // Heart icon (bottom right)
+                  Positioned(
+                    bottom: 10,
+                    right: 10,
+                    child: Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            _likeStates[index]
+                                ? Icons.favorite
+                                : Icons.favorite_border,
+                          ),
+                          color: _likeStates[index] ? Colors.red : Colors.grey,
+                          onPressed: () {
+                            setState(() {
+                              _likeStates[index] = !_likeStates[index];
+                              if (_likeStates[index]) {
+                                _likeCounts[index]++;
+                              } else {
+                                _likeCounts[index]--;
+                              }
+                            });
+                          },
+                        ),
+                        Text('${_likeCounts[index]}'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    ],
-  );
-}
-
+      ],
+    );
+  }
 }
